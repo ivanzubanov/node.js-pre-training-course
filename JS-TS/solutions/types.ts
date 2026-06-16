1c5ff1c2c9a23103e07d2fa406615460386772f2
Ivan Zubanov <ivanzubanovwork@gmail.com>
2026-06-16 10:43:53 +0300

    completed task-01

--- JS-TS/solutions/types.ts
+++ JS-TS/solutions/types.ts
@@ -1,9 +1,17 @@
 interface Todo {
-
+  id: number;
+  title: string;
+  description?: string;
+  status: TodoStatus;
+  readonly createdAt: Date;
 }
 
 enum TodoStatus {
-
+  PENDING = "PENDING",
+  IN_PROGRESS = "IN_PROGRESS",
+  COMPLETED = "COMPLETED",
 }
 
-export { Todo, TodoStatus };
+type NewTodo = Omit<Todo, "id" | "createdAt">;
+
+export { Todo, TodoStatus, NewTodo };
