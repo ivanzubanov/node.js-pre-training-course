30664629486485d48edc82d25c5764edf1284c61
Ivan Zubanov <ivanzubanovwork@gmail.com>
2026-06-20 16:33:25 +0300

    Fully completed task 2

--- JS-TS/solutions/array-helpers.ts
+++ JS-TS/solutions/array-helpers.ts
@@ -22,7 +22,18 @@ export function filterArray<T>(
   source: readonly T[],
   predicate: (item: T, index: number) => boolean,
 ): T[] {
-  throw new Error("filterArray: not implemented");
+  // throw new Error("filterArray: not implemented");
+  if (source == null) {
+    throw new TypeError("Source array cannot be null or undefined");
+  }
+  let result: T[] = [];
+  for (let i: number = 0; i < source.length; i++) {
+    let elem: T = source[i];
+    if (predicate(elem, i)) {
+      result.push(elem);
+    }
+  }
+  return result;
 }
 
 export function reduceArray<T, R>(
@@ -30,19 +41,52 @@ export function reduceArray<T, R>(
   reducer: (acc: R, item: T, index: number) => R,
   initial: R,
 ): R {
-  throw new Error("reduceArray: not implemented");
+  // throw new Error("reduceArray: not implemented");
+  if (source == null) {
+    throw new TypeError("Source array cannot be null or undefined");
+  }
+  let acc: R = initial;
+  for (let i: number = 0; i < source.length; i++) {
+    acc = reducer(acc, source[i], i);
+  }
+  return acc;
 }
 
 export function partition<T>(
   source: readonly T[],
   predicate: (item: T) => boolean,
 ): [T[], T[]] {
-  throw new Error("partition: not implemented");
+  // throw new Error("partition: not implemented");
+  if (source == null) {
+    throw new TypeError("Source array cannot be null or undefined");
+  }
+  let arrayTrue: T[] = [];
+  let arrayFalse: T[] = [];
+  for (let i: number = 0; i < source.length; i++) {
+    if (predicate(source[i])) {
+      arrayTrue.push(source[i]);
+    } else {
+      arrayFalse.push(source[i]);
+    }
+  }
+  return [arrayTrue, arrayFalse];
 }
 
 export function groupBy<T, K extends PropertyKey>(
   source: readonly T[],
   keySelector: (item: T) => K,
 ): Record<K, T[]> {
-  throw new Error("groupBy: not implemented");
+  // throw new Error("groupBy: not implemented");
+  if (source == null) {
+    throw new TypeError("Source array cannot be null or undefined");
+  }
+  let result = {} as Record<K, T[]>;
+  for (let i: number = 0; i < source.length; i++) {
+    const key = keySelector(source[i]);
+    if (!result[key]) {
+      result[key] = [];
+    }
+    result[key].push(source[i]);
+  }
+  return result;
 }
